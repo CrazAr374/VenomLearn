b88713ed9944c9283c3e9feaaa314e8a79764a9f
Atharva374 <atharva.rahate374@gmail.com>
2025-06-16 15:45:12 +0530

    Added the description on PyPi website

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from setuptools import setup, find_packages
+import pathlib
+
+# Read the contents of README.md for long_description
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="VenomLearn",
@@ -14,6 +19,8 @@ setup(
     author="VenomLearn Team",
     author_email="example@example.com",
     description="An interactive Python learning package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     keywords="python, learning, interactive, education",
     url="https://github.com/VenomLearn/VenomLearn",
     classifiers=[
